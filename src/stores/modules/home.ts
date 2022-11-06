fee1ab3b3880a0313c2921ceea32f1440fa93451
Ethan <1028716801@qq.com>
2022-11-06 14:25:14 +0800

    封装的axios + pinia + 类型声明文件的使用

--- src/stores/modules/home.ts
+++ src/stores/modules/home.ts
@@ -1,12 +1,21 @@
 import { defineStore } from "pinia";
 // 类型声明问件的应用
 import type { CategoryList } from "@/types";
+// 二次封装axios的使用
+import http from "@/utils/request";
 
 const useHomeStore = defineStore('home', {
     state: () => {
         return {
             msg: 'vue3-ts-pinia模板',
             categoryList: [] as CategoryList,
+        }
+    },
+    actions: {
+        async getAllCategory() {
+            // 使用
+            const res = await http<CategoryList>('get', '/home/category/head')
+            this.categoryList = res.data.result
         }
     }
 });
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,14 +3,24 @@
         home-page
         <div>{{ home.msg }}</div>
         <div>{{ auto }}</div>
+        <div>使用二次封装axios请求数据</div>
+        <ul>
+            <li v-for="item in home.categoryList" :key="item.id">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
 
 <script setup lang="ts">
 import useStore from '@/stores'
 const { home } = useStore()
+
 // 可以不用再引入 ref 等api，直接使用
 const auto = ref('自动引入api')
+
+// 使用pinia发请求(二次封装的axios)，以及管理数据
+onMounted(() => {
+    home.getAllCategory()
+})
 </script>
 
 <style scoped lang="less">
