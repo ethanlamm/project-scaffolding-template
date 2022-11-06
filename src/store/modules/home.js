fde5581559d81be3b60af869d09006a7f4a63080
Ethan <1028716801@qq.com>
2022-11-06 18:18:39 +0800

    封装axios + 使用pinia请求数据

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,9 +1,18 @@
 import { defineStore } from "pinia";
+import http from '@/utils/request'
 
 const useHomeStore = defineStore('home', {
     state: () => {
         return {
             msg: 'vue3-js-pinia模板',
+            categoryList: []
+        }
+    },
+    actions: {
+        async getAllCategory() {
+            // 使用  默认get请求
+            const res = await http('/home/category/head')
+            this.categoryList = res.result
         }
     }
 });
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,6 +3,10 @@
         home-page
         <div>{{ auto }}</div>
         <div>{{ home.msg }}</div>
+        <div>使用二次封装axios请求数据</div>
+        <ul>
+            <li v-for="item in home.categoryList" :key="item.id">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
 
@@ -10,6 +14,11 @@
 import useStore from '@/store'
 const auto = ref('api自动引入')
 const { home } = useStore()
+
+// 使用pinia发请求(二次封装的axios)，以及管理数据
+onMounted(() => {
+    home.getAllCategory()
+})
 </script>
 
 <style scoped lang="less">
