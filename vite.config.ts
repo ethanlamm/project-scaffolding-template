bca2a31ba9a9bfc4a7db1fa753d22384734e3bd8
Ethan <1028716801@qq.com>
2022-11-06 11:12:43 +0800

    vite.config配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+// 1. Jsx 插件，初始化项目的时候选择Jsx就会自动下载
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  base: './',  // 设置打包路径
+
+  plugins: [
+    vue(
+      {
+        // 响应性语法糖目前默认是关闭状态，需要你显式选择启用
+        // reactivityTransform: true,
+      }
+    ),
+    // 2. 应用 vueJsx 插件
+    vueJsx()
+  ],
+
+  // 配置开发服务器
+  server: {
+    port: 8080,
+    // cors:true,     // 允许开发时 ajax 跨域
+    // open:true,     // 打开浏览器
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
