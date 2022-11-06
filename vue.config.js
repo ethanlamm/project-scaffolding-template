045ee37505fb71e41a3ec7161fd6e09090d6dd64
Ethan <1028716801@qq.com>
2022-11-06 18:47:01 +0800

    vue.config配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: './',
+  outputDir: 'dist',
+  // 放置生成的静态资源 
+  assetsDir: 'static',
+  // 生产环境的 source map
+  productionSourceMap: false,
 })
