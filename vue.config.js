94274692ecace74cf69c2a5ab3b26464cae16179
Ethan <1028716801@qq.com>
2022-11-06 15:04:00 +0800

    vue.config配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',   // 放置生成的静态资源 
+  productionSourceMap: false,   // 生产环境的 source map
+  devServer: {
+    // 配置代理跨域
+    proxy: {
+      '/dev-api': {
+        target: 'http://pcapi-xiaotuxian-front-devtest.itheima.net/',
+        pathRewrite: { "^/dev-api": "" }
+      }
+    }
+  }
 })
