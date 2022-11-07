3a7b31b29877881e55cae416f39c4892ae922908
Ethan <1028716801@qq.com>
2022-11-07 11:42:46 +0800

    vite.config配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
+// 引入path
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  // 公共基础路径，默认'/'，一般不需要改动
+  base: '/',
+
+  // 服务器配置
+  server: {
+    // 原端口号 5173
+    port: 8080
+  },
+
+  // src别名配置
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
